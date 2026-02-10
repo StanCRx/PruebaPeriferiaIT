6e5778a3a0bb6600c35efd8caa37b19cfa8b03a9
DESKTOP-JPDTOK8\Stan <josestalyn@hotmail.com>
2026-02-10 11:32:31 -0500

    Configuracion de tanstackQuery y rutas

---
+++ src/app/router.tsx
@@ -0,0 +1,6 @@
+import { createBrowserRouter } from "react-router";
+import { ProductsPage } from "../features/products/pages";
+
+export const router = createBrowserRouter([
+  { path: "/", element: <ProductsPage /> },
+]);
